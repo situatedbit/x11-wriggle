a6eca14b0df5f6726ffa07d91e7db0c0a313bb5b
Matt Schaefer <matt@situatedbit.com>
2018-12-16 09:08:37 -0800

    stretch can compress or expand either left or right side

--- lib/stretch.rb
+++ lib/stretch.rb
@@ -1,18 +1,62 @@
 require_relative 'wmctrl_command'
 
 class Stretch
+  Action = Struct.new(
+      :column_offset_left,
+      :column_offset_right,
+      :snap_offset_left,
+      :snap_offset_right
+  )
+
+  def self.compress
+    # Snap to current column, then bump column number to move sides inward
+    Action.new(1, -1, 0, 0)
+  end
+
+  def self.expand
+    # Rely only on snap offsets to move sides outward
+    Action.new(0, 0, -1, 1)
+  end
+
+  class Right
+    def self.left(window_left, layout, action)
+      window_left
+    end
+
+    def self.right(window_right, layout, action)
+      # Apply snap offset, then offset the column number
+      column = layout.column_index(window_right + action.snap_offset_right)
+
+      layout.columns[column + action.column_offset_right].right
+    end
+  end
+
+  class Left
+    def self.left(window_left, layout, action)
+      # Apply snap offset, then offset the column number
+      column = layout.column_index(window_left + action.snap_offset_left)
+
+      layout.columns[column + action.column_offset_left].left
+    end
+
+    def self.right(window_right, layout, action)
+        window_right
+    end
+  end
+
   def initialize(params = {})
+    @action = params.fetch(:action, self.class.expand)
     @window = params.fetch(:window)
-    @to_right = params.fetch(:to_right, true)
+    @side = params.fetch(:side)
     @layout = params.fetch(:layout)
   end
 
   def left
-    @to_right ? @window.left : @layout.column(@window.left - 1).left
+    @side.left @window.left, @layout, @action
   end
 
   def right
-    @to_right ? @layout.column(@window.right + 1).right : @window.right
+    @side.right @window.right, @layout, @action
   end
 
   def width
--- spec/stretch_spec.rb
+++ spec/stretch_spec.rb
@@ -5,14 +5,16 @@ describe :Stretch do
   describe :left do
     let(:layout) { ColumnLayout.new(width: 100, count: 10) }
 
-    it 'will be window left when direction is right' do
-      window = instance_double('Window', left: 39)
+    context 'side is right' do
+      it 'will be window left' do
+        window = instance_double('Window', left: 39)
 
-      expect(Stretch.new(window: window, layout: layout).left).to eq(39)
+        expect(Stretch.new(window: window, layout: layout, side: Stretch::Right).left).to eq(39)
+      end
     end
 
-    context 'when direction is left' do
-      let(:params) { { layout: layout, to_right: false } }
+    context 'side is left' do
+      let(:params) { { layout: layout, to_right: false, side: Stretch::Left } }
 
       it 'will be 0 if window left is negative' do
         window = instance_double('Window', :left => -10)
@@ -43,20 +45,38 @@ describe :Stretch do
 
         expect(Stretch.new(params.merge(window: window)).left).to eq(90)
       end
+
+      context 'compressing' do
+        let(:compressing_params) { params.merge action: Stretch.compress }
+
+        it 'will be one column to the right if window left is in middle of column' do
+          window = instance_double('Window', :left => 15)
+
+          expect(Stretch.new(compressing_params.merge(window: window)).left).to eq(20)
+        end
+
+        it 'will be one column to the right if window left is at the column start' do
+          window = instance_double('Window', :left => 20)
+
+          expect(Stretch.new(compressing_params.merge(window: window)).left).to eq(30)
+        end
+      end
     end
   end
 
   describe :right do
     let(:layout) { ColumnLayout.new(width: 100, count: 10) }
 
-    it 'will be window right when direction is left' do
-      window = instance_double('Window', right: 39)
+    context 'side is left' do
+      it 'will be window right when direction is left' do
+        window = instance_double('Window', right: 39)
 
-      expect(Stretch.new(window: window, layout: layout, to_right: false).right).to eq(39)
+        expect(Stretch.new(window: window, layout: layout, side: Stretch::Left).right).to eq(39)
+      end
     end
 
-    context 'when direction is right' do
-      let(:params) { { layout: layout, to_right: true } }
+    context 'side is right' do
+      let(:params) { { layout: layout, side: Stretch::Right } }
 
       it 'will be end of column 1 if window right is negative' do
         window = instance_double('Window', :right => -10)
@@ -87,10 +107,22 @@ describe :Stretch do
 
         expect(Stretch.new(params.merge(window: window)).right).to eq(99)
       end
+
+      context 'compressing' do
+        let(:compressing_params) { params.merge action: Stretch.compress }
+
+        it 'will be one column to the left if window right is in middle of column' do
+          window = instance_double('Window', :right => 45)
+
+          expect(Stretch.new(compressing_params.merge(window: window)).right).to eq(39)
+        end
+
+        it 'will be one column to the left if window right is at the column end' do
+          window = instance_double('Window', :right => 39)
+
+          expect(Stretch.new(compressing_params.merge(window: window)).right).to eq(29)
+        end
+      end
     end
   end
-
-  describe :width
-
-  describe :height
 end
